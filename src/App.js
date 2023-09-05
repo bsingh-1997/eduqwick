e39b71b50a0423cbc14b168e9ef04d549da3dba2
barinder <barindersingh1997@gmail.com>
2023-09-06 00:43:07 +0530

    networkerrorcommit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-       <Routes>
+       <Routes> 
              <Route path="/" element={<Home/>} ></Route>
           </Routes>
   
